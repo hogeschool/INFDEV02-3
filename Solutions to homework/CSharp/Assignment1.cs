b2d3176b7f11eda065cf9738f71614d7c688bc6d
giuseppemag <giuseppemag@gmail.com>
2016-01-11 12:56:27 +0100

    Added week 1

---
+++ Solutions to homework/CSharp/Assignment1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+  class Counter
+  {
+    int cnt, tgt;
+    Action onTarget;
+
+    public Counter()
+    {
+      cnt = 0;
+      tgt = -1;
+    }
+
+    public Counter(int startFrom)
+    {
+      cnt = startFrom;
+      tgt = -1;
+    }
+
+    public void Tick()
+    {
+      cnt = cnt + 1;
+      if (cnt == tgt)
+        onTarget();
+    }
+
+    public void Reset()
+    {
+      cnt = 0;
+    }
+
+    public static Counter operator +(Counter c1, Counter c2)
+    {
+      return new Counter(c1.cnt + c2.cnt);
+    }
+
+    public void OnTarget(int tgt, Action onTarget)
+    {
+      this.tgt = tgt;
+      this.onTarget = onTarget;
+    }
+
+    public override string ToString()
+    {
+      return "Count is " + cnt;
+    }
+  }
+}
---
+++ Solutions to homework/CSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Assignment1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TestAssignment1();
+    }
+
+    private static void TestAssignment1()
+    {
+      var c0 = new Counter();
+      c0.Tick();
+      var c = new Counter() + c0;
+      c.OnTarget(50, () => Console.WriteLine("Reached 50!"));
+      for (int i = 0; i < 100; i++)
+      {
+        c.Tick();
+      }
+      Console.WriteLine(c);
+    }
+  }
+}
