877a5a10190b571fe3732fd4ee41b6f473d8512c
giuseppemag <giuseppemag@gmail.com>
2016-01-11 13:42:48 +0100

    Assignment 2.

---
+++ Solutions to homework/CSharp/Assignment2 - advanced.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+  interface List<T>
+  {
+    int Length { get; }
+    void Iter(Action<T> f);
+    List<U> Map<U>(Func<T, U> f);
+    List<T> Filter(Func<T, bool> p);
+  }
+
+  class Empty<T> : List<T>
+  {
+    public int Length
+    {
+      get
+      {
+        return 0;
+      }
+    }
+
+    public List<T> Filter(Func<T, bool> p)
+    {
+      return new Empty<T>();
+    }
+
+    public void Iter(Action<T> f)
+    {
+    }
+
+    public List<U> Map<U>(Func<T, U> f)
+    {
+      return new Empty<U>();
+    }
+  }
+
+  class Node<T> : List<T>
+  {
+    T head;
+    List<T> tail;
+    public Node(T x, List<T> xs) { head = x; tail = xs; }
+
+    public int Length
+    {
+      get
+      {
+      return 1 + tail.Length;
+      }
+    }
+
+    public List<T> Filter(Func<T, bool> p)
+    {
+      if (p(head))
+        return new Node<T>(head, tail.Filter(p));
+      else
+        return tail.Filter(p);
+    }
+
+    public void Iter(Action<T> f)
+    {
+      f(head);
+      tail.Iter(f);
+    }
+
+    public List<U> Map<U>(Func<T, U> f)
+    {
+      return new Node<U>(f(head), tail.Map(f));
+    }
+  }
+
+}
--- Solutions to homework/CSharp/Program.cs
+++ Solutions to homework/CSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Assignment1;
+using Assignment2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +7,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharp
-{
+{  
   class Program
   {
     static void Main(string[] args)
     {
-      TestAssignment1();
+      //TestAssignment1();
+      //TestAssignment2(new Car(), new Truck(), new Enterprise());
+      TestAssignment2Advanced(new Node<Vehicle>(new Car(), new Node<Vehicle>(new Truck(), new Node<Vehicle>(new Enterprise(), new Empty<Vehicle>()))));
+    }
+
+    private static void TestAssignment2(Vehicle v1, Vehicle v2, Vehicle v3)
+    {
+      v1.LoadFuel(new Gasoline(5));
+      v2.LoadFuel(new Dilithium(1000));
+      v3.LoadFuel(new Dilithium(3));
+      for (int i = 0; v1.Move(); i++)
+        Console.WriteLine("Vehicle one is still going strong after " + i + " steps");
+      for (int i = 0; v2.Move(); i++)
+        Console.WriteLine("Vehicle two is still going strong after " + i + " steps");
+      for (int i = 0; v3.Move(); i++)
+        Console.WriteLine("Vehicle three is still going strong after " + i + " steps");
+    }
+
+    private static void TestAssignment2Advanced(Assignment2.List<Vehicle> vehicles)
+    {
+      vehicles.Iter(v => v.LoadFuel(new Gasoline(100)));
+      vehicles.Iter(v => v.LoadFuel(new Diesel(100)));
+      vehicles.Iter(v => v.LoadFuel(new Dilithium(100)));
+      for (int i = 0; i < 99; i++)
+        vehicles.Iter(v => v.Move());
+      vehicles.Iter(v => Console.WriteLine(v));
     }
 
     private static void TestAssignment1()
