6206ae60554148e571aa80cb3df4019fdbcc9596
Youri Tjang <mail@tjang.nl>
2016-02-01 09:21:24 +0100

    Merge branch 'master' of /Users/youritjang/IdeaProjects/Courses/infdev02-3_v4 with conflicts.

--- Slides/Program.fs
+++ Slides/Program.fs
@@ -5,7 +5,7 @@
     do System.IO.File.WriteAllText(tex, source |> SlideDefinition.generateLatexFile title)
     for i = 0 to (if compileTwice then 1 else 0) do
 //      let p = System.Diagnostics.ProcessStartInfo("pdflatex.exe", "-synctex=1 -interaction=nonstopmode " + tex)
-      let p = System.Diagnostics.ProcessStartInfo("pdflatex.exe", "-synctex=1 -interaction=batchmode " + tex)
+      let p = System.Diagnostics.ProcessStartInfo("pdflatex", "-synctex=1 -interaction=batchmode " + tex)
       do p.UseShellExecute <- false
       do (System.Diagnostics.Process.Start p).WaitForExit()
     do System.Console.ReadLine() |> ignore
