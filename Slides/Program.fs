be106b8e9155f5191d28bd746d5b694a9f15667b
mabbadi <mabbadi@outlook.com>
2016-03-29 22:33:38 +0200

    Sample exams updated

--- Slides/Program.fs
+++ Slides/Program.fs
@@ -9,8 +9,8 @@ let main argv =
 //  do batchProcess LatexDefinition.generateDocument Dev2.SampleExams.exam4 "INFDEV02_2_sample_exam1" "The INFDEV team" "Sample exam 4" true false
 //  do batchProcess LatexDefinition.generateDocument Dev2.SampleExams.exam5 "INFDEV02_2_sample_exam1" "The INFDEV team" "Sample exam 5" true false
 
-//  do batchProcess LatexDefinition.generateDocument Dev3.SampleExams.exam1 "INFDEV02_3_sample_exam1" "The INFDEV team" "Sample exam 1" true false
-  do batchProcess LatexDefinition.generateDocument Dev3.SampleExams.exam2 "INFDEV02_3_sample_exam2" "The INFDEV team" "Sample exam 2" true false
+  do batchProcess LatexDefinition.generateDocument Dev3.SampleExams.exam1 "INFDEV02_3_sample_exam1" "The INFDEV team" "Sample exam 1" true false
+  //do batchProcess LatexDefinition.generateDocument Dev3.SampleExams.exam2 "INFDEV02_3_sample_exam2" "The INFDEV team" "Sample exam 2" true false
 //  do batchProcess LatexDefinition.generateDocument Dev3.SampleExams.exam3 "INFDEV02_3_sample_exam3" "The INFDEV team" "Sample exam 3" true false
 
 //  do batchProcess LatexDefinition.generatePresentation Chapter1.Week1_2.slides "INFDEV02_3_Lec1_OO_intro" "The INFDEV team" "Introduction" false true
