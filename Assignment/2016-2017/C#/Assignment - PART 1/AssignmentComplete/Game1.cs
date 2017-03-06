bd3030b4c61a9b1f8397ad11a7e40d4ef7fd95d7
mabbadi <mabbadi@outlook.com>
2017-03-06 13:12:30 +0100

    Fixed bugs related to content pipeline

--- Assignment/2016-2017/C#/Assignment - PART 1/AssignmentComplete/Game1.cs
+++ Assignment/2016-2017/C#/Assignment - PART 1/AssignmentComplete/Game1.cs
@@ -29,14 +29,14 @@ namespace AssignmentComplete
     protected override void LoadContent()
     {
       spriteBatch = new SpriteBatch(graphics.GraphicsDevice);
-      Texture2D volvo = Content.Load<Texture2D>("volvo.png");
-      Texture2D mine = Content.Load<Texture2D>("mine.png");
-      Texture2D ore_container = Content.Load<Texture2D>("ore_container.png");
-      Texture2D product_container = Content.Load<Texture2D>("product_container.png");
-      Texture2D ikea = Content.Load<Texture2D>("ikea.png");
-      Texture2D background = Content.Load<Texture2D>("background.png");
-      Texture2D mine_cart = Content.Load<Texture2D>("mine_cart.png");
-      Texture2D product_box = Content.Load<Texture2D>("product_box.png");
+      Texture2D volvo = Content.Load<Texture2D>("volvo");
+      Texture2D mine = Content.Load<Texture2D>("mine");
+      Texture2D ore_container = Content.Load<Texture2D>("ore_container");
+      Texture2D product_container = Content.Load<Texture2D>("product_container");
+      Texture2D ikea = Content.Load<Texture2D>("ikea");
+      Texture2D background = Content.Load<Texture2D>("background");
+      Texture2D mine_cart = Content.Load<Texture2D>("mine_cart");
+      Texture2D product_box = Content.Load<Texture2D>("product_box");
       gameState = new GameState(background, mine_cart, product_box, volvo, mine, ikea, ore_container, product_container);
     }
 
